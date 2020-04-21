b6a9fdff8e93757c405071fe66eb7b773a05f1d3
Angel Betancourt <angel.betancourt-ext@block.one>
2020-04-21 09:18:16 -0500

    minor fix

--- challenge_001_abetancourt.playground/Contents.swift
+++ challenge_001_abetancourt.playground/Contents.swift
@@ -1,8 +1,6 @@
 import UIKit
 import XCTest
 
-var str = "Hello, playground"
-
 //### Simple Adding
 //Have the function simpleAdding(num) add up all the numbers from 1 to num.
 //For example: if the input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10.
@@ -20,8 +18,7 @@ final class UserManagerTests: XCTestCase {
         
         let receivedResult = simpleAdding(num: givenNumber)
         
-        let expectedResutlt = (1...givenNumber).reduce(into: 0) { $0 += $1 }
-        XCTAssertEqual(receivedResult, expectedResutlt, "The received result is not valid")
+        XCTAssertEqual(receivedResult, 6, "The received result is not valid")
     }
 }
 
